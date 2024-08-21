837ff8acaca70a4d1b2f53da5dffc14b883dc369
DimiCT <dimitri.griparis@ctengineeringgroup.com>
2024-08-21 13:33:25 +0300

    Update main.tf
    node size from small to medium

--- AWAKE-EKS/main.tf
+++ AWAKE-EKS/main.tf
@@ -79,7 +79,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
@@ -89,7 +89,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
